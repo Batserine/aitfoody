7baaee7ee767d7c743ad22b68345070d3495a001
st119983@ait.asia <st119983@ait.asia>
2018-09-15 16:41:51 +0700

    new file:   assignment1/app/views/basics/basics_controller.rb 	new file:   assignment1/app/views/basics/import_xml.html.erb
    modified

---
+++ assignment1/app/views/basics/basics_controller.rb
@@ -0,0 +1,19 @@
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# On branch master
+# Your branch and 'origin/master' have diverged,
+# and have 4 and 2 different commits each, respectively.
+#   (use "git pull" to merge the remote branch into yours)
+#
+# Changes to be committed:
+	modified:   basics_controller.rb
+#
+# Changes not staged for commit:
+#	modified:   ../views/assignment/index.html.erb
+#	modified:   ../../config/routes.rb
+#
+# Untracked files:
+#	../views/basics/import_xml.html.erb
+#
