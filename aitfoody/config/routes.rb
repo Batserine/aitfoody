12900a9e6b84a0c9c6b1b4050403c94bcbc1d0d3
st119983@ait.asia <st119983@ait.asia>
2018-12-01 20:35:49 +0700

    small changes

--- aitfoody/config/routes.rb
+++ aitfoody/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   get 'myarticles/index'
   # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount RailsAdmin::Engine => '/admin', :as => :rails_admin
