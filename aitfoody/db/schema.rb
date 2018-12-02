16556b21eeedd711feab9a10d767714624e4b80a
st119983@ait.asia <st119983@ait.asia>
2018-12-02 12:19:36 +0700

    schema

--- aitfoody/db/schema.rb
+++ aitfoody/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_101517) do
+ActiveRecord::Schema.define(version: 2018_11_29_170433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,18 +29,12 @@ ActiveRecord::Schema.define(version: 2018_11_30_101517) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "relations", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "following_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "revarticles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.integer "rating"
     t.integer "price"
+    t.string "keywords"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -96,7 +90,8 @@ ActiveRecord::Schema.define(version: 2018_11_30_101517) do
     t.string "firstname"
     t.string "lastname"
     t.string "sex"
-    t.boolean "active_status", default: false
+    t.string "ban_status"
+    t.boolean "active_status", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
