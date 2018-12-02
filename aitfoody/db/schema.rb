9730bc326a1dac46f57f882f82c587ae42781841
st119983@ait.asia <st119983@ait.asia>
2018-12-02 12:45:56 +0700

    schema

--- aitfoody/db/schema.rb
+++ aitfoody/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_11_29_170433) do
-=======
 ActiveRecord::Schema.define(version: 2018_12_01_173043) do
->>>>>>> 61a0233abefe1a966f4b4a06bb174f0a1b214b6c
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,8 +30,6 @@ ActiveRecord::Schema.define(version: 2018_12_01_173043) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
-=======
   create_table "places", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -44,14 +38,6 @@ ActiveRecord::Schema.define(version: 2018_12_01_173043) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "relations", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "following_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
->>>>>>> 61a0233abefe1a966f4b4a06bb174f0a1b214b6c
   create_table "revarticles", force: :cascade do |t|
     t.string "title"
     t.text "content"
