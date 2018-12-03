34619a6cceae4018633dc93e861d398776d9a585
Balakrishna <balu636216@gmail.com>
2018-12-04 03:31:12 +0700

    entire project

--- aitfoody/test/controllers/backend_controller_test.rb
+++ aitfoody/test/controllers/backend_controller_test.rb
@@ -2,10 +2,8 @@ require 'test_helper'
 
 class BackendControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
-  # test "the truth" do
-  #   assert true
-  # end
- setup do
+  
+  setup do
     sign_in users(:one)
   end
 end
--- aitfoody/test/controllers/comments_controller_test.rb
+++ aitfoody/test/controllers/comments_controller_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class CommentsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
   setup do
-
     @comment = comments(:one)
     sign_in users(:one)
   end
